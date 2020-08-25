5fe5b80516d334e462b94272227e9598ee00b402
Becca <60694049+olive49@users.noreply.github.com>
2020-08-25 22:04:57 +0300

    nav bar wip

--- tree-whisperer/src/App.js
+++ tree-whisperer/src/App.js
@@ -1,4 +1,5 @@
 import React, { useCallback, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Classifier from "./components/Classifier.jsx";
 import Dropzone from "./components/DropZone.jsx";
@@ -11,31 +12,34 @@ import axios from "axios";
 function App() {
   const [images, setImages] = useState([]);
 
-  const uploadImage = (file) => {
-    console.log(file);
-    const formData = new FormData();
-    formData.append("file", file);
-    try {
-      const analyze_response = axios
-        .post("http://127.0.0.1:5000/test_pic", formData)
-        .then((data) => {
-          console.log(data);
-        });
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  // const uploadImage = (file) => {
+  //   console.log(file);
+  //   const formData = new FormData();
+  //   formData.append("file", file);
+  //   try {
+  //     const analyze_response = axios
+  //       .post("http://127.0.0.1:5000/test_pic", formData)
+  //       .then((data) => {
+  //         console.log(data);
+  //       });
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
 
   return (
     <div className="App">
-      <div className="Background">
-        {" "}
-        <NavBar />
-      </div>
-      <div>
-        {/* <CameraFeed sendFile={uploadImage} /> */}
-        <Classifier />
-      </div>
+      <Router>
+        <div className="Background">
+          <NavBar />
+        </div>
+        <Switch>
+          <Route path="/welcome" exact></Route>
+          <Route path="/classifier" exact>
+            <Classifier />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
