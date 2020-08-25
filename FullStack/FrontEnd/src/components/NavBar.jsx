879749b5a805f75db9e435e9c6043d25201a16af
Becca <60694049+olive49@users.noreply.github.com>
2020-08-25 23:23:20 +0300

    nav bar wip

--- FullStack/FrontEnd/src/components/NavBar.jsx
+++ FullStack/FrontEnd/src/components/NavBar.jsx
@@ -2,35 +2,37 @@ import React from "react";
 import { Link, withRouter, useLocation } from "react-router-dom";
 
 const NavBar = () => {
+  const navStyle = {
+    textDecoration: "none",
+  };
+
+  const listStyle = {
+    padding: "2rem",
+    color: "black",
+  };
+
+  const navUl = {
+    listStyle: "none",
+    display: "flex",
+    flexDirection: "row",
+    paddingLeft: 0,
+  };
+
   return (
     <nav style={{ display: "flex" }}>
-      <div>
+      <div style={{ border: "2px solid red" }}>
         <img
-          style={{ height: "110%", margin: "0.7rem" }}
+          style={{ height: "100%", margin: "0.7rem" }}
           src={require("../images/logo-vector The Tree Whisperer.png")}
         />
       </div>
-      <div>
-        <ul className="nav_ul">
-          <Link to="/">
-            <li
-              style={{
-                padding: "2rem",
-                color: "black",
-              }}
-            >
-              Home
-            </li>
+      <div style={{ width: "80%", border: "2px solid red" }}>
+        <ul style={navUl}>
+          <Link to="/" style={navStyle}>
+            <li style={listStyle}>Home</li>
           </Link>
-          <Link to="classifier">
-            <li
-              style={{
-                padding: "2rem",
-                color: "black",
-              }}
-            >
-              Classifier
-            </li>
+          <Link to="classifier" style={navStyle}>
+            <li style={listStyle}>Classifier</li>
           </Link>
         </ul>
       </div>
