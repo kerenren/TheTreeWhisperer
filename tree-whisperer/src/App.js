457509eccf6f831a47f7366ffb6f883ce839ee94
Becca <60694049+olive49@users.noreply.github.com>
2020-08-25 22:37:53 +0300

    nav bar wip

--- tree-whisperer/src/App.js
+++ tree-whisperer/src/App.js
@@ -2,30 +2,12 @@ import React, { useCallback, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Classifier from "./components/Classifier.jsx";
-import Dropzone from "./components/DropZone.jsx";
-import ImageList from "./components/ImageList.jsx";
 import NavBar from "./components/NavBar.jsx";
-import CameraFeed from "./components/camera-feed.jsx";
 import cuid from "cuid";
 import axios from "axios";
 
 function App() {
   const [images, setImages] = useState([]);
-
-  // const uploadImage = (file) => {
-  //   console.log(file);
-  //   const formData = new FormData();
-  //   formData.append("file", file);
-  //   try {
-  //     const analyze_response = axios
-  //       .post("http://127.0.0.1:5000/test_pic", formData)
-  //       .then((data) => {
-  //         console.log(data);
-  //       });
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
 
   return (
     <div className="App">
@@ -34,7 +16,7 @@ function App() {
           <NavBar />
         </div>
         <Switch>
-          <Route path="/welcome" exact></Route>
+          <Route path="/" exact></Route>
           <Route path="/classifier" exact>
             <Classifier />
           </Route>
--- tree-whisperer/src/components/NavBar.jsx
+++ tree-whisperer/src/components/NavBar.jsx
@@ -1,12 +1,39 @@
 import React from "react";
+import { Link, withRouter, useLocation } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav>
-      <img
-        style={{ height: "110%", margin: "0.7rem" }}
-        src={require("../images/logo-vector The Tree Whisperer.png")}
-      />
+    <nav style={{ display: "flex" }}>
+      <div>
+        <img
+          style={{ height: "110%", margin: "0.7rem" }}
+          src={require("../images/logo-vector The Tree Whisperer.png")}
+        />
+      </div>
+      <div>
+        <ul className="nav_ul">
+          <Link to="/">
+            <li
+              style={{
+                padding: "2rem",
+                color: "black",
+              }}
+            >
+              Home
+            </li>
+          </Link>
+          <Link to="classifier">
+            <li
+              style={{
+                padding: "2rem",
+                color: "black",
+              }}
+            >
+              Classifier
+            </li>
+          </Link>
+        </ul>
+      </div>
     </nav>
   );
 };
