fcafe484d8ebd7774a51ff3ef5308c62edf0e340
Kelly <renkelei.kelly@gmail.com>
2020-08-25 15:12:41 +0300

    updated uuid as photo name

--- FullStack/BackEnd/DataLayer/BaseDataLayer.py
+++ FullStack/BackEnd/DataLayer/BaseDataLayer.py
@@ -1,11 +1,10 @@
 from werkzeug.utils import secure_filename
 import os
-from flask import request
 
 
 class BaseDataLayer:
     def __init__(self):
-        self.ALLOWED_EXTENSIONS = set(['jpg','png'])
+        self.ALLOWED_EXTENSIONS = set(['jpg', 'png'])
 
     def add_plant(self):
         pass
@@ -23,10 +22,12 @@ class BaseDataLayer:
         return '.' in filename and \
                filename.rsplit('.', 1)[1] in self.ALLOWED_EXTENSIONS
 
-    def add_leaf_to_fs(self, image, app, UPLOAD_FOLDER):
+    def add_leaf_to_fs(self, image, app, UPLOAD_FOLDER, unique_id):
         print(image)
         if image and self.allowed_file(image.filename):
-            filename = secure_filename(image.filename)
+            old_filename = secure_filename(image.filename)
+            old_file_ext = old_filename.split(".", 1)[1]
+            filename = str(unique_id) + f'.{old_file_ext}'
             image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            filepath = f'{UPLOAD_FOLDER}/{filename}'
+            filepath = f'{UPLOAD_FOLDER}{filename}'
             return filepath
